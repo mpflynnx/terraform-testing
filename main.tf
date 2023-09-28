93e89b1c9249c1695c9823f5010c5ec41dc00c83
Matthew Flynn <mpflynnx@gmail.com>
2023-09-28 13:24:14 +0000

    refactor as branch 23-config-drift

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
-resource "random_string" "bucket_name" {
-  length  = 32
-  special = false
-  upper = false
-}
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
 
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
   tags = {
     UserUuid = var.user_uuid
   }
-  
 }
