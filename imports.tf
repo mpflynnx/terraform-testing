9d98df0c660cb8ff770d4810447e4c6973286bfc
Matthew Flynn <mpflynnx@gmail.com>
2023-09-29 09:18:06 +0000

    import existing S3 bucket

---
+++ imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_s3_bucket.website_bucket
+  id = "terraform-20230928133942847400000001"
+}
--- providers.tf
+++ providers.tf
@@ -1,10 +1,4 @@
 terraform {
-  cloud {
-    organization = "mpflynnx"
-    workspaces {
-      name = "testing-workspace"
-    }
-   }
   required_providers {
     aws = {
       source = "hashicorp/aws"
